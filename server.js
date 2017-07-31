e4e8a1cf2ca5d5af89a30c1a0f1fb85488eb1879
Abhilash360 <abhilash.kadupatil@gmail.com>
2017-07-31 18:18:58 -0400

    [GameOn] Added Package and server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+var app = require('./express');
+var express = app.express;
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static(__dirname + '/public'));
+
+port = process.env.PORT || 3000;
+app.listen(port);
